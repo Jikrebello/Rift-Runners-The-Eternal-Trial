6010927ee2f9401a67362db7853b5468989e3018
James Rebello <Jikrebello.za@gmail.com>
2023-11-11 16:41:33 +0200

    Added movement via direction input.

--- Test/PlayerController/StateMachine/Locomotion/LocomotionState.cs
+++ Test/PlayerController/StateMachine/Locomotion/LocomotionState.cs
@@ -1,15 +1,18 @@
 ﻿using Stride.Core.Mathematics;
 using Stride.Engine.Events;
 using Stride.Input;
+using System;
 using System.Collections.Generic;
 
 namespace Test.PlayerController.StateMachine.Locomotion
 {
     public class LocomotionState : IState
     {
-        protected Vector3 inputDirection;
         protected EventReceiver<bool> aimingReceiver;
         protected bool isAiming;
+        protected Vector3 inputDirection;
+        protected Vector3 moveDirection;
+        protected float maxSpeed = 2;
         public PlayerContext Context { get; set; }
 
         public virtual void Enter(Dictionary<string, object> parameters)
@@ -36,6 +39,10 @@ namespace Test.PlayerController.StateMachine.Locomotion
             );
 
             inputDirection = GetInputDirection();
+
+            moveDirection = inputDirection * maxSpeed;
+
+            Context.Character.SetVelocity(moveDirection);
         }
 
         public virtual void Exit()
@@ -47,8 +54,8 @@ namespace Test.PlayerController.StateMachine.Locomotion
         {
             Vector3 inputDirection =
                 new(
-                    (Context.Input.IsKeyDown(Keys.D) ? 1 : 0)
-                        - (Context.Input.IsKeyDown(Keys.A) ? 1 : 0),
+                    (Context.Input.IsKeyDown(Keys.A) ? 1 : 0)
+                        - (Context.Input.IsKeyDown(Keys.D) ? 1 : 0),
                     0.0f,
                     (Context.Input.IsKeyDown(Keys.W) ? 1 : 0)
                         - (Context.Input.IsKeyDown(Keys.S) ? 1 : 0)
@@ -57,6 +64,20 @@ namespace Test.PlayerController.StateMachine.Locomotion
             inputDirection.Normalize();
 
             return inputDirection;
+        }
+
+        private Quaternion LookAt(Vector3 moveDirection)
+        {
+            float yawOrientation = 0;
+
+            if (moveDirection.Length() > 0.001)
+            {
+                yawOrientation = MathUtil.RadiansToDegrees(
+                    (float)Math.Atan2(-moveDirection.Z, moveDirection.X) + MathUtil.PiOverTwo
+                );
+            }
+
+            return Quaternion.RotationYawPitchRoll(MathUtil.DegreesToRadians(yawOrientation), 0, 0);
         }
     }
 }
